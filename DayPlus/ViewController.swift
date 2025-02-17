8a243f12cc5933283f4fe4876080557bf5cd75da
kimilko <kimsho.dev@gmail.com>
2025-02-17 21:21:35 +0900

    First commit

---
+++ DayPlus/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  DayPlus
+//
+//  Created by 김승호 on 2/17/25.
+//
+
+import UIKit
+import WebKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var webView: WKWebView!
+    
+    override func viewDidLoad() {
+        webView.isInspectable = true
+        
+        // 웹페이지 로드 (웹앱 주소 변경 가능)
+        if let url = URL(string: "http://devsun.work:18080") {
+            webView.load(URLRequest(url: url))
+        }
+    }
+}
