765a2e6e02cf980f3a561e4416dbf6ca589275cb
kimilko <67352774+kimilko@users.noreply.github.com>
2025-04-16 15:15:56 +0900

    TODO notification

--- DayPlus/ViewController.swift
+++ DayPlus/ViewController.swift
@@ -18,5 +18,51 @@ class ViewController: UIViewController {
         if let url = URL(string: "http://devsun.work:18080") {
             webView.load(URLRequest(url: url))
         }
+        
+        // https://velog.io/@wannabe_eung/UserNotifications-Framework%EB%A5%BC-%EC%95%8C%EC%95%84%EB%B3%B4%EC%9E%90
+        
+        let center = UNUserNotificationCenter.current()
+        center.requestAuthorization(options: [.alert, .sound, .badge]) { granted, error in
+            
+            if let error = error {
+                // Handle the error here.
+            }
+            
+            // Enable or disable features based on the authorization.
+        }
+        
+        let content = UNMutableNotificationContent()
+        content.title = "Weekly Staff Meeting"
+        content.body = "Every Tuesday at 2pm"
+        
+        
+        
+        // Configure the recurring date.
+        var dateComponents = DateComponents()
+        dateComponents.calendar = Calendar.current
+
+        dateComponents.weekday = 4  // Tuesday
+        dateComponents.hour = 15    // 14:00 hours
+        dateComponents.minute = 13
+           
+        // Create the trigger as a repeating event.
+        let trigger = UNCalendarNotificationTrigger(
+                 dateMatching: dateComponents, repeats: true)
+        
+        
+        
+        
+        // Create the request
+        let uuidString = UUID().uuidString
+        let request = UNNotificationRequest(identifier: uuidString,
+                    content: content, trigger: trigger)
+
+        // Schedule the request with the system.
+        let notificationCenter = UNUserNotificationCenter.current()
+        notificationCenter.add(request) { (error) in
+           if error != nil {
+              // Handle any errors.
+           }
+        }
     }
 }
